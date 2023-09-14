17ca17bc8934cf25beb25baca34b3e5d01635e75
SeungJae Son <46300191+Sonny-Kor@users.noreply.github.com>
2023-09-14 14:35:22 +0900

    ConstraintLayout

--- W02/app/src/main/java/com/example/w02/MainActivity.kt
+++ W02/app/src/main/java/com/example/w02/MainActivity.kt
@@ -6,14 +6,23 @@ import com.example.w02.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var main: ActivityMainBinding
+    private var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
         main = ActivityMainBinding.inflate(layoutInflater)
         setContentView(main.root)
-        main.btnAdd.setOnClickListener {
-            main.txtCount.text = "눌렀습니다."
-        }
+//        main.btnAdd.setOnClickListener {
+//            count += 1
+//            main.txtCount.text = count.toString()
+//        }
+//        main.btnSubtract.setOnClickListener{
+//            if(count > 0)
+//                main.txtCount.text = "${--count}"
+//        }
+//        main.btnReset.setOnClickListener {
+//            count = 0
+//            main.txtCount.text = count.toString()
+//        }
     }
-
 }
