c80f8339c9cb86cc9fb45098190c287b3eee549b
SeungJae Son <46300191+Sonny-Kor@users.noreply.github.com>
2023-09-21 14:33:39 +0900

    📌 LifeCycle Study

--- W03_Lotto/app/src/main/java/com/SJ_RESPECT/w03_lotto/MainActivity.kt
+++ W03_Lotto/app/src/main/java/com/SJ_RESPECT/w03_lotto/MainActivity.kt
@@ -2,6 +2,8 @@ package com.SJ_RESPECT.w03_lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.LogPrinter
 import com.SJ_RESPECT.w03_lotto.databinding.ActivityMainBinding
 import kotlin.random.Random
 
@@ -9,6 +11,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var main : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Log.i("Lifecycle !!!", "onCreate()")
 //        setContentView(R.layout.activity_main)
         main = ActivityMainBinding.inflate(layoutInflater) // infate 는 객체를 메모리에 올리는것
         setContentView(main.root)
@@ -23,5 +27,36 @@ class MainActivity : AppCompatActivity() {
             main.num6.text = Random.nextInt(1,46).toString()
 
         }
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.i("Lifecycle !!!", "onStart()")
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("Lifecycle !!!", "onResume()")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("Lifecycle !!!", "onRestart()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("Lifecycle !!!", "onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("Lifecycle !!!", "onStop()")
+    }
+
+    override fun onDestroy() {
+
+        super.onDestroy()
+        Log.i("Lifecycle !!!", "onDestroy()")
     }
 }
