14eeda75d0c1d79d4505b44272407631f291b817
SeungJae Son <46300191+Sonny-Kor@users.noreply.github.com>
2023-10-05 14:11:47 +0900

    Add LiveData

--- W03_Lotto/app/src/main/java/com/SJ_RESPECT/w03_lotto/LottoViewModel.kt
+++ W03_Lotto/app/src/main/java/com/SJ_RESPECT/w03_lotto/LottoViewModel.kt
@@ -1,28 +1,33 @@
 package com.SJ_RESPECT.w03_lotto
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlin.random.Random
 
 class LottoViewModel : ViewModel( ) {
-    //private var _numbers = intArrayOf(0, 0, 0, 0, 0, 0)
-    private var _numbers = IntArray(6) { 0 }
-    val numbers
+
+    private var _numbers = MutableLiveData<IntArray>(IntArray(6) { 0 })
+
+    val numbers: LiveData<IntArray>
         get() = _numbers
     fun generate() {
         var num = 0
-        for (i in _numbers.indices) {
-//            _numbers[i] = Random.nextInt(1, 46)
+        val newNumbers = IntArray(6){ 0 }
+
+        for (i in newNumbers.indices) {
             // 중복 검사
             do {
                 num = Random.nextInt(1, 46)
-            } while (_numbers.contains(num))
-            _numbers[i] = num
+            } while (newNumbers.contains(num))
+            newNumbers[i] = num
 
-            Log.i("Number!!!", _numbers[i].toString())
+            Log.i("Number!!!", newNumbers[i].toString())
         }
 
         // 정렬
-        _numbers.sort()
+        newNumbers.sort()
+        _numbers.value = newNumbers
     }
 }
--- W03_Lotto/app/src/main/java/com/SJ_RESPECT/w03_lotto/MainActivity.kt
+++ W03_Lotto/app/src/main/java/com/SJ_RESPECT/w03_lotto/MainActivity.kt
@@ -7,13 +7,13 @@ import android.util.LogPrinter
 import com.SJ_RESPECT.w03_lotto.databinding.ActivityMainBinding
 import kotlin.random.Random
 import android.widget.TextView
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
     private lateinit var main: ActivityMainBinding
     private lateinit var model: LottoViewModel
     private lateinit var txtNum: Array<TextView?>
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.i("Lifecycle!!!", "onCreate")
@@ -23,29 +23,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(main.root)
 
         txtNum = arrayOf(main.num1, main.num2, main.num3, main.num4, main.num5, main.num6)
-//        txtNum[0] = main.num1
-//        txtNum[1] = main.num2
-//        txtNum[2] = main.num3
-//        txtNum[3] = main.num4
-//        txtNum[4] = main.num5
-//        txtNum[5] = main.num6
 
         model = ViewModelProvider(this)[LottoViewModel::class.java]
-        txtNum.forEachIndexed { index, textView ->
-            textView?.text = model.numbers[index].toString()
-        }
+        setNumbersText()
+
+        model.numbers.observe(this, Observer {
+            Log.i("Observer!!!", "numbers changed")
+            setNumbersText()
+        })
 
         main.btnGenerate.setOnClickListener {
             model.generate()
-            txtNum.forEachIndexed { index, textView ->
-                textView?.text = model.numbers[index].toString()
-            }
-//            main.num1.text = model.numbers[0].toString()
-//            main.num2.text = model.numbers[1].toString()
-//            main.num3.text = model.numbers[2].toString()
-//            main.num4.text = model.numbers[3].toString()
-//            main.num5.text = model.numbers[4].toString()
-//            main.num6.text = model.numbers[5].toString()
+            //setNumbersText()
+        }
+    }
+
+    private fun setNumbersText() {
+        txtNum.forEachIndexed { index, textView ->
+            textView?.text = model.numbers.value!![index].toString()
         }
     }
 
